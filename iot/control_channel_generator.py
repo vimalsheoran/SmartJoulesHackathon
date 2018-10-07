31a5c3ea7fbc6a48d487ff91e64fd87cad4d3e8e
Vimal Sheoran <vimalpsheoran@gmail.com>
2018-10-08 00:58:47 +0530

    Added a script to generate control point channels

---
+++ iot/control_channel_generator.py
@@ -0,0 +1,26 @@
+from channel_generator import CONFIG, generate_control_channel, generate_feedback_channel
+import json
+import requests
+
+COMPONENT_DATA_MAP = {}
+
+components_list = (requests.get(CONFIG["components"])).json()
+
+for component in components_list:
+	COMPONENT_DATA_MAP.update({
+				str(component["name"]): {}
+	})
+	new_data = component["controls"]
+	for control_pt, pt_id in new_data.iteritems():
+		control_channel = generate_control_channel(str(pt_id))
+		feedback_channel = generate_feedback_channel(str(pt_id))
+
+		COMPONENT_DATA_MAP[component["name"]].update({
+			str(control_pt): {"id": str(pt_id),
+							"channel": control_channel,
+							"feedback": feedback_channel
+			}
+		})
+
+with open('control_channel.json', 'w') as file:
+	json.dump(COMPONENT_DATA_MAP, file)
