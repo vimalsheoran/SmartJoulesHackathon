1318f77eb66003d87dea0cf9d2049ecce2fd4073
Vimal Sheoran <vimalpsheoran@gmail.com>
2018-10-15 10:59:54 +0530

    Added mqtt functionality to the middleware

--- iot/iot_middleware.py
+++ iot/iot_middleware.py
@@ -1,5 +1,6 @@
 from server_config import SERVER_CONFIG
 from channel_handler import ChannelFinder
+from iot_client import IotClient
 
 import socket
 import json
@@ -8,6 +9,10 @@ DATA_CHANNELS = json.load(open("data_channel.json","r"))
 CONTROL_CHANNELS = json.load(open("control_channel.json", "r"))
 
 CHANNEL_FINDER = ChannelFinder(DATA_CHANNELS, CONTROL_CHANNELS)
+
+IOT_CLIENT = IotClient()
+IOT_CLIENT.connect_broker()
+IOT_CLIENT.start_service()
 
 iot_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -21,8 +26,26 @@ while True:
 	if not data:
 		break
 	else:
-		CHANNEL_FINDER.process_command(data)
+		comm_type, command = CHANNEL_FINDER.process_command(data)
 		channel = CHANNEL_FINDER.find_channel()
-	incoming_conn.send(channel)
+	if comm_type == "subscribe":
+		incoming_conn.send(str(IOT_CLIENT.subscribe(channel)))
+	elif comm_type == "publish":
+		incoming_conn.send(str(IOT_CLIENT.publish(channel, command)))
 
+IOT_CLIENT.stop_service()
 iot_socket.close()
+
+# Test channels
+
+#### Subscribe #####
+
+# data/Chilled Water Pump 1/data/EM/146
+
+#### Publish ####
+
+# control/Chilled Water Pump 1/command/start/69
+
+#### Feedback ####
+
+# control/Chilled Water Pump 1/feedback/start/69
