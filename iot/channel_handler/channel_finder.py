1b4c14fe25be2c31a2028abb6308bd40c6bb7118
Vimal Sheoran <vimalpsheoran@gmail.com>
2018-10-15 10:58:25 +0530

    Removed debugging statement, renaming variables to make more sense.

--- iot/channel_handler/channel_finder.py
+++ iot/channel_handler/channel_finder.py
@@ -5,21 +5,31 @@ class ChannelFinder(object):
 		self.component = ""
 		self.device = ""
 		self.device_id = ""
-		self.channel_type = ""
+		self.command = ""
 		self.channel = ""
 		self.data_channels = data_channels
 		self.control_channels = control_channels
 
 	def process_command(self, command_string):
+		
+		print "processing command........."
 		command_buffer = command_string.split('/')
 		self.operation = command_buffer[0]
 		self.component = command_buffer[1]
-		self.channel_type = command_buffer[2]
+		self.command = command_buffer[2]
 		self.device = command_buffer[3]
 		self.device_id = command_buffer[4]
 
+		if self.operation == "data":
+			return "subscribe", ""
+		elif self.operation == "control" and self.command == "feedback":
+			return "subscribe", ""
+		else:
+			return "publish", self.command
+
 	def find_channel(self):
 		
+		print "generating channel........."
 		if self.operation == "data":
 			dataset = self.data_channels
 			return self.data_channel_finder(dataset)
@@ -31,8 +41,6 @@ class ChannelFinder(object):
 
 	def data_channel_finder(self, dataset):
 		for component, devices in dataset.iteritems():
-			print self.component
-			print component
 			if component == self.component:
 				for device, details in devices.iteritems():
 					if device == self.device and details["id"] == self.device_id:
@@ -42,18 +50,11 @@ class ChannelFinder(object):
 
 	def control_channel_finder(self, dataset):
 		for component, devices in dataset.iteritems():
-			print self.component
-			print component
 			if component == self.component:
 				for device, details in devices.iteritems():
 					if device == self.device and details["id"] == self.device_id:
-						if self.channel_type == "feedback":
+						if self.command == "feedback":
 							self.channel = details["feedback"]
 						else:
 							self.channel = details["channel"]
-		return self.channel		
-
-# sample command structure
-# data/Chilled Water Pump 1/data/EM/146
-# sample control command structure
-# control/Chilled Water Pump 1/feedback/start/69
+		return self.channel
